1b36d3ba4ff9f26ba95565141a461d39b13b3986
LY001369 <LY001369@gmail.com>
2023-10-17 10:26:09 +0700

    update test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,9 +2,7 @@ import sys, os
 # Thêm đường dẫn đến thư mục chứa my_module vào sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(current_dir))
+import app
 
-from app import analysis
-
-text = "cho em hỏi quy chế tín chỉ là gì"
-a = analysis.load_stopwords()
-print(a)
+text = "cho em hỏi quy chế tín chỉ là gì?"
+print(app.get_keys(text))
