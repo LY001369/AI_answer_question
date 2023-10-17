5069dc7e9376dd1ee3c98228614abc45daf43496
LY001369 <LY001369@gmail.com>
2023-10-17 09:33:54 +0700

    update module test

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+import sys, os
+# Thêm đường dẫn đến thư mục chứa my_module vào sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(current_dir))
--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,7 +1,3 @@
-import sys, os
-# Thêm đường dẫn đến thư mục chứa my_module vào sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(current_dir))
 
 from app import analysis
 
