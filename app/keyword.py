dce0b20a137965f10ade64e057736f9410ae0bcf
LY001369 <LY001369@gmail.com>
2023-10-17 14:04:40 +0700

    add keyword.py

--- app/keyword.py
+++ app/keyword.py
@@ -16,16 +16,19 @@ def get_keys(quest):
     stopwords = load_stopwords()
 
     # Phân tách từ
-    #tokenized_text = ViTokenizer.tokenize(quest).split()
+    tokenized_text = ViTokenizer.tokenize(quest)
 
-    # Loại bỏ stop words
-    #words = [word for word in tokenized_text if word.lower() not in stopwords]
+    # Phân loại từ 
+    tagged_word = ViPosTagger.postagging(tokenized_text)
 
-    # Phân loại từ thành danh từ riêng, động từ, tính từ và danh từ
-    tagged_word = ViPosTagger.pos_tag(quest)
+    # chon cac loai tu co the lam key
     type_word = ["N", "Ny", "V", "A"]
     keys = [word[0] for word in tagged_word if word[1] in type_word]
 
+    # Loại bỏ stop words
+    keys = [word for word in tagged_word if word.lower() not in stopwords]
+
     return keys
+
 if __name__ == "__main__":
     print(load_stopwords())
--- tests/test_analysis.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-# Thêm đường dẫn đến thư mục chứa my_module vào sys.path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(current_dir))
-
-from app.analysis import get_keys
-
-text = "cho em hỏi quy chế tín chỉ là gì?"
-print(get_keys(text))
