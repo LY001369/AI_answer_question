59f888bc9a66a9cf8767d1dd81a25ed2080a72ec
LY001369 <LY001369@gmail.com>
2023-10-17 10:47:22 +0700

    update test

--- app/analysis.py
+++ app/analysis.py
@@ -1,10 +1,10 @@
 from pyvi import ViPosTagger, ViTokenizer
-import json
+import json, os
 
 # Hàm tải lên stopword 
 def load_stopwords():
     # Đường dẫn đến tệp JSON chứa danh sách stop words
-    file_path = "./data/rss/stopword_vi.json"
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'rss/stopword_vi.json')
 
     # Mở tệp JSON và nạp dữ liệu
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -27,3 +27,5 @@ def get_keys(quest):
     keys = [word[0] for word in tagged_word if word[1] in type_word]
 
     return keys
+if __name__ == "__main__":
+    print(load_stopwords())
