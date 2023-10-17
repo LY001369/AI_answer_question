fc0d812a34da2e2591023ea73263c6f53c1b3d24
LY001369 <LY001369@gmail.com>
2023-10-17 10:28:24 +0700

    update test

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,7 +2,8 @@ import sys, os
 # Thêm đường dẫn đến thư mục chứa my_module vào sys.path
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(current_dir))
-import app
+
+from app.analysis import get_keys
 
 text = "cho em hỏi quy chế tín chỉ là gì?"
 print(app.get_keys(text))
